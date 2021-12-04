9a2d5b73e117d53954b9f6671da5e555b0cc0b13
pIlIp-d <62812264+pIlIp-d@users.noreply.github.com>
2021-12-04 23:21:35 +0100

    feature improvements and bugfixes
    -complete redo of vertical move
    -add inactive arrows to move_with_arrows
    -added parameter for initial preset

--- javascript/dashboard_move.js
+++ javascript/dashboard_move.js
@@ -12,7 +12,6 @@
 		grid[pos[pos.length-1]].pos = swap_1;
 	}
 
-
 	function gridsize_at_id(size_,id_){
 		for (s in size_){//more than one returns disjunct
 			if (grid[id_].size === s)
@@ -26,226 +25,198 @@
 		else
 			return false;	
 	}
+	function get_above(id){
+		for (ele=id;ele>=0;ele--){
+			if (grid[id].start-4 === grid[ele].start || 
+				(grid[id].start-8 === grid[ele].start && grid[ele].size === "24")||
+				(grid[id].start-12 === grid[ele].start && grid[ele].size === "34") ||
+				(grid[id].start-16 === grid[ele].start && grid[ele].size === "44"))
+				return ele;
+		}
+		return -1;
+	}
 
 	//---- UP ----
+	function swap_two_with_one(id,ele){
+		let swap1 = grid[id].pos;//"12"over"11","11" or "13"over "11","12" or big over "11","13"/"12","12" 
+		for (i=0;i<id-ele-1;i++)
+			grid[id-i].pos = grid[id-1-i].pos;
+		grid[Number(ele)+1].pos = grid[id+1].pos;
+		grid[id+1].pos = grid[ele].pos;
+		grid[ele].pos = swap1;
+	}
 	function click_up(id){
+		console.log("up");
 		id = Number(id);
 		var changed = false;
-		if (view === "vertical" && id != 0){
-			if (width_is_four(id)){
-				console.log("big");
-				changed = true;
-				switch(grid[id-1].size){
-					case "14":
-					case "24":
-					case "44":
-					case "34":
-						swap([id, id-1]);//big one over big one	
-						break;
-					case "11":
-						if (grid[id-2].size === "11" && grid[id-3].size === "11")
-							swap([id,id-4,id-3,id-2,id-1]);//"11","11","11","11"
-						else if(grid[id-2].size === "13")
-							swap([id,id-2,id-1]);//11,13
-						else
-							swap([id,id-3,id-2,id-1]);//"12","11","11" and //"11","12","11"
-						break;
-					case "12":
-						if (grid[id-2].size === "12")
-							swap([id,id-2,id-1]);//"12","12"
-						else
-							swap([id,id-3,id-2,id-1]);//"11","11","12"
-						break;
-					case "13":
-						swap([id,id-2,id-1]);//11,13
-						break;
-					default:
-						changed = false;
-				}
-			}
-			for (ele=id;ele>=0;ele--){//until 1 because size of grid isnt involved and it cant be below 0
-				if (grid[id].start-4 === grid[ele].start || 
-					(grid[id].start-8 === grid[ele].start && grid[ele].size === "24")||
-					(grid[id].start-12 === grid[ele].start && grid[ele].size === "34") ||
-					(grid[id].start-16 === grid[ele].start && grid[ele].size === "44")){//ele is above id
-					console.log("ele: "+ele);
-					if (grid[id].size === grid[ele].size){
-						changed = true;
-						swap([id,ele]);
-						console.log("same ele");
-					}
-					switch(grid[id].size){
-						case "11":
-							changed = true;
-							if (grid[ele].size === "12" && grid[id+1].size === "11" ||
-								grid[ele].size === "13" && grid[id+1].size === "12"){
-								let swap1 = grid[id].pos;//"12"over"11","11" or "13"over "11","12" or big over "11","13" 
-								for (i=0;i<id-ele-1;i++)
-									grid[id-i].pos = grid[id-1-i].pos;
-								grid[Number(ele)+1].pos = grid[id+1].pos;
-								grid[id+1].pos = grid[ele].pos;
-								grid[ele].pos = swap1;
-							}	
-							else if (width_is_four(ele) && grid[id+1].size === "13")
-								swap([id+1,id,ele]);		
-							else if(grid[ele].size === "13" && grid[id+1].size === "11" && grid[id+2].size === "11")
-								swap([ele,id+2,id+1,id]);
-							else if(width_is_four(ele) && grid[id+1].size === "11" && grid[id+2].size === "12"||
-									width_is_four(ele) && grid[id+1].size === "12" && grid[id+2].size === "11"){
-								swap([ele,id+2,id]);
-								swap([id-1,id+1]);
-							}
-							else if (grid[ele].size === "12" && grid[ele+1].size === "12" && grid[id+1].size === "13" ||
-									 grid[ele].size === "13" && grid[ele+1].size === "11" && grid[id+1].size === "13"){
-								swap([ele,id]);
-								swap([ele+1,id+1]);
-							}
-							else
-								changed = false;
-							break;
-						case "12":
-							changed = true;
-							if (grid[ele].size === "11" && grid[Number(ele)+1].size === "11"){
-								changed = true;//"11","11"over"12"
-								let swap1 = grid[id].pos;
-								grid[id].pos = grid[Number(ele)+1].pos;
-								for (i=1;i<id-ele-1;i++)
-									grid[Number(ele)+i].pos = grid[Number(ele)+i+1].pos;
-								grid[id-1].pos = grid[ele].pos;
-								grid[ele].pos = swap1;
-							}
-							else if(grid[ele].size === "11" && grid[Number(ele)+1].size === "12" && grid[id+1].size === "11" ||
-									grid[ele].size === "11" && grid[Number(ele)+1].size === "13" && grid[id+1].size === "12" ||
-									grid[ele].size === "13" && grid[1+ele].size === "11" && grid[1+id].size === "12"){
-									swap([ele,id]);
-									swap([ele+1,id+1]);
-							}
-							else if (width_is_four(ele) && grid[id+1].size === "11" && grid[id+2] === "11")
-								swap([ele,id+2,id+1,id]);
-							else if (width_is_four(ele) && grid[id+1].size === "12")
-								swap([ele,id+1,id]);
-							else if (grid[ele].size === "13" && grid[id+1].size === "11")
-								swap([ele,id+1,id-1,id]);
-							else
-								changed = false;
-							break;
-						case "13":
-							changed = true;
-							if (grid[id-1].size === "11"){			
-								if (id >= 4 && grid[id-2].size === "11" && grid[id-3].size === "11" && grid[id-4].size === "11"){
-									swap([id-2,ele,id]);
-									swap([id-1,Number(ele)+1]);
-								}
-								else if (id >= 3 && (grid[id-2].size === "12" && grid[id-3].size === "11" || grid[id-2].size === "11" && grid[id-3].size === "12"))
-									swap([ele,id-1,Number(ele)+1,id]);
-								else
-									changed = false;
-							}
-							else if (width_is_four(ele) && grid[id+1].size === "11")
-								swap([ele,id+1,id]);
-							else if (grid[ele].size === "12" && grid[ele+1].size === "12" && grid[id+1].size === "11" ||
-									grid[ele].size === "11" && grid[ele+1].size === "13" && grid[id+1].size === "11"){
-									swap([ele,id]);
-									swap([ele+1,id+1]);
-							}
-							else if (grid[id+1].size === "11" && grid[ele].size === "11" && grid[ele+1].size === "11" && grid[ele+2].size === "12")
-								swap([ele,id,ele+1,id+1,ele+2]);
-							else 
-								changed = false;
-							break;
-					}
-				}
-				else if (!changed && (grid[id].start != 1 && grid[id].start-5 === grid[ele].start || 
-									  (grid[id].start == 3 || grid[id].start % 4 == 0) && grid[id].start-6 === grid[ele].start || 
-									  grid[id].start == 0 && grid[id].start-7 === grid[ele].start)){
+		if (view === "vertical"){
+			var ele = get_above(id);
+			changed = true;
+			console.log(ele);
+			if (ele === -1){
+				if (grid[id].start % 4 != 1)
 					click_up(id-1);
-					console.log("loop");
-					changed = false;
-				}
+				return;
 			}
+			else if (grid[id].size === grid[ele].size)
+				swap([ele,id]);
+			else if ("11" === grid[ele].size && "12" === grid[ele+1].size && //special case
+					"12" === grid[id].size && "11" === grid[id+1].size ||
+					"11" === grid[id].size && "12" === grid[id+1].size &&
+					"12" === grid[ele].size && "11" === grid[ele+1].size){
+				swap([id,ele]);//12,11 over 11,12 
+				swap([id+1,ele+1]);
+			}
+			else if (grid[id].size > grid[ele].size && grid[ele+1].size != "13" || grid[id].size == "11" && grid[id+1].size == "13"){//13 is part of special cases
+				click_down(ele);
+				return;
+			}
+			else if ("11" === grid[ele].size && "13" === grid[ele+1].size || 
+					"13" === grid[ele].size && "11" === grid[ele+1].size){
+				//overlaps 11,13 -- 12,12/13,11 or 12,11,11
+				if (grid[id].start+3 === grid[id+1].stop){//two
+					swap([id,ele]);
+					swap([id+1,ele+1]);
+				}
+				else if (grid[id].start+3 === grid[id+2].stop)//three
+					swap([ele,id+1,ele+1,id+2,id]);
+				else change = false;
+			}
+			else if ("12" === grid[ele].size){
+				if (grid[id].start+1 === grid[id+1].stop){//two in row
+					var swap1 = grid[ele].pos;
+					grid[ele].pos = grid[id+1].pos;
+					grid[id+1].pos = grid[ele+1].pos;
+					grid[ele+1].pos = grid[ele+2].pos;
+					if (ele-id === 4)
+						grid[ele+2].pos = grid[ele+3].pos;
+					grid[id].pos = swap1;
+				}
+				else change = false;
+			}
+			else if ("13" === grid[ele].size){
+				if (grid[id].start+2 === grid[id+1].stop)//two in row
+					swap([id,ele+1,id+1,ele]);
+				else if (grid[id].start+2 === grid[id+2].stop){//three in row
+					swap([ele,id,id+2]);
+					swap([ele+1,id+1]);
+				}
+				else change = false;
+			}
+			else if (width_is_four(ele)){
+				if (grid[id+3] != null && grid[id].start+3 === grid[id+3].stop)//four in row
+					swap([ele,id+3,id+2,id+1,id]);
+				else if(grid[id+2] != null && grid[id].start+3 === grid[id+2].stop)//three in row
+					swap([ele,id+2,id+1,id]);
+				else if(grid[id].start+3 === grid[id+1].stop)//two in row
+					swap([ele,id+1,id]);
+				else change = false;
+			}
+			else if (!changed && (grid[id].start % 4 != 1)){//sideways loop though until aligns with above
+				click_up(id-1);
+				return;
+			}
+			else changed = false;
 		}
-		else if (view === "horizontal"){
+		else if (view === "horizontal"){//todo überarbeiten
 			for (ele=id;ele>=0;ele--){
 				console.log("ele_c: "+ele);
-				if (grid[id].start-8 === grid[ele].start){	
-					click_down(ele);
-					ele=-1;
-				}
-				else if(grid[id].start-9 === grid[ele].start){
-					click_down(ele);
-					ele=-1;
-				}	
-				else if(grid[id].start-10 === grid[ele].start){
-					click_down(ele);
-					ele=-1;
-				}	
-				else if(grid[id].start-11 === grid[ele].start){
-					click_down(ele);
-					ele=-1;
-				}	
+				if (grid[id].start-8 === grid[ele].start)
+					click_down(ele--);
+				else if(grid[id].start-9 === grid[ele].start)
+					click_down(ele--);
+				else if(grid[id].start-10 === grid[ele].start)
+					click_down(ele--);
+				else if(grid[id].start-11 === grid[ele].start)
+					click_down(ele--);
 			}
 		}
 		if (changed)
 			grid.update();
 	}
 
+	function get_below(id){
+		for (ele=id;ele<grid.length;ele++){
+			if (grid[id].start+4 === grid[ele].start || 
+				(grid[id].start+8 === grid[ele].start && grid[ele].size === "24")||
+				(grid[id].start+12 === grid[ele].start && grid[ele].size === "34") ||
+				(grid[id].start+16 === grid[ele].start && grid[ele].size === "44"))
+				return ele;
+		}
+		return -1;
+	}
 	//---- Down ----
 	function click_down(id){	
 		console.log("down");
 		id = Number(id);
 		var changed = false;
 		if (view === "vertical"){
-			var num = get_row_below(id);//amount of items in next row
-			var start = start_of_next_row(id);
-			if (id < grid.length-1){
-				if (grid[id].size === "14" || grid[id].size === "24" ||grid[id].size === "44" || grid[id].size === "34"){
-				 	changed = true;
-				 	if (num === 1)
-				 		swap([id,id+1]); 
-				 	else if (num === 2)
-						swap([id,id+2,id+1]);
-					else if (num === 3)//"12","11","11"
-						swap([id,id+3,id+2,id+1]);
-					else if (num === 4)
-				 		swap([id,id+4,id+3,id+2,id+1]);
-					else 
-					 	changed = false;
-				}
-				else {
-					for (ele=id;ele<grid.length;ele++){
-						if (grid[ele].start-4 === grid[id].start){
-				 			changed = true;	
-							if (grid[ele].size === grid[id].size)
-								swap([id,ele]);//"11" over "11" or "12"over "12"
-							else 
-								click_up(ele);
-						}
-						else if (grid[id].start === grid[ele].start-3 && grid[id].size === "11" && grid[id-1].size === "11" && grid[ele].size === "12"){
-							click_down(id-1);//"11","11" over "12" and theh right one pressed
-							changed = false;					
-						}
-						else if (grid[ele].stop-4 === grid[id].stop && grid[id].size === "13" && grid[id+1].size === "11"){
-							changed = true;						
-							if (grid.length > id+3 && grid[id+2].size == "12" && grid[id+3].size == "11" || grid[id+2].size == "11" && grid[id+3].size == "12")
-								swap([id+1,ele-1,id,ele]);//13 over \n 	"11","12"or"12","11"
-							else if (grid.length > id+4 && grid[id+2].size == "11" && grid[id+3].size == "11" && grid[id+4].size == "11" ){
-								swap([id+1,ele-1]);//13 over \n 3x"11"
-								swap([id+2,id,ele]);
-							}
-							else changed = false;
-						}
-					} 
-				}
+			var ele = get_below(id);
+			console.log(ele);
+
+			if (ele === -1){ 
+				if (grid[id].start % 4 != 1)
+					click_down(id-1);
+				return;
 			}
+			changed = true;
+			if (grid[id].size === grid[ele].size)
+				swap([ele,id]);
+			if (grid[id].size > grid[ele].size && grid[ele+1].size != "13" || grid[id].size == "11" && grid[id+1].size == "13"){
+				click_up(ele);
+				return;
+			}
+			else if ("11" === grid[ele].size && "13" === grid[ele+1].size || 
+					"13" === grid[ele].size && "11" === grid[ele+1].size){
+				//overlaps 11,13 -- 12,12/13,11 or 12,11,11
+				if (grid[id].start+3 === grid[id+1].stop){//two
+					swap([id,ele]);
+					swap([id+1,ele+1]);
+				}
+				else if (grid[id].start+3 === grid[id+2].stop)//three
+					swap([ele,id+1,ele+1,id+2,id]);
+				else change = false;
+			}
+			else if ("12" === grid[ele].size)
+				if (grid[id].start+1 === grid[id+1].stop){//two in row
+					var swap1 = grid[ele].pos;
+					grid[ele].pos = grid[id].pos;
+					grid[id].pos = grid[id+2].pos;
+					if (id-ele === 4)
+						grid[id+2].pos = grid[ele-1].pos;
+					grid[ele-1].pos = grid[id+1].pos;
+					grid[id+1].pos = swap1;
+				}
+				else if(grid[ele+1].size == "11" && grid[id].size == "11" && grid[id+1].size == "12"){
+					swap([id,ele]);
+					swap([id+1,ele+1]);
+				}
+				else change = false;
+			else if ("13" === grid[ele].size){
+				if (grid[id].start+2 === grid[id+1].stop)//two in row
+					swap([ele,id,id+1,id+2]);
+				else if (grid[id].start+2 === grid[id+2].stop)//three in row
+					swap([ele,id,id+1,id+2,id+3]);
+				else change = false;
+			}
+			else if (width_is_four(ele)){
+				if (grid[id].start+3 === grid[id+3].stop)//four in row
+					swap([ele,id,id+1,id+2,id+3]);
+				else if(grid[id].start+3 === grid[id+2].stop)//three in row
+					swap([ele,id,id+1,id+2]);
+				else if(grid[id].start+3 === grid[id+1].stop)//two in row
+					swap([ele,id,id+1]);
+				else change = false;
+			}
+			else changed = false;
 		}
 		else if (view === "horizontal"){
 			for (ele=id;ele<grid.length;ele++){
 				if (grid[id].start === grid[ele].start-8){//id above ele 
 					console.log("normal"+ele);
 					changed = true;
-					if (grid[id].size === grid[ele].size){
+					if (grid[id].size === grid[ele].size)
 						swap([ele,id]);
-					}
 			//------11------
 					else if (grid[id].size === "11" && (grid[ele].size === "12" && grid[id+1].size === "11" || grid[ele].size === "13" && grid[id+1].size === "12" || grid[ele].size === "14" && grid[id+1].size === "13")){ 
 						//11,11 \n 12 --or-- 11,12 \n 13
@@ -446,10 +417,8 @@
 				}
 			}
 		}
-						
 		if (changed)
 			grid.update();
-		
 	}
 
 	function start_of_next_row(id){	//checks for each first position of a row:i 
@@ -510,24 +479,10 @@
 	function click_left(id){
 		id = Number(id);
 		if (id > 0){
+			var changed = false;
 			if (view === "vertical"){
-				if (grid[id].size === "14" || grid[id].size === "24" || grid[id].size === "44" || grid[id].size === "34")
-					click_up(id);
-			 	var changed = true;
-			 	if ((grid[id].size === grid[id-1].size) ||//2x gleich
-			 		(grid[id].size === "11" && grid[id-1].size === "12" && grid[id].start % 4 != 1) ||//"12","11" and "11" not completely left
-			 		(grid[id].size === "12" && grid[id-1].size === "11" && grid[id].start % 4 != 1))//"11","12" and "12" not completely left
-					swap([id,id-1]);
-				else if (id > 2 && grid[id].size === "12" && grid[id-1].size === "11" && grid[id-2].size === "11")
-					swap([id-2, id-1, id]);//id 12 and left or the right 2 in row above are 11
-				else if (id > 1 && grid[id].size === "11" && grid[id+1].size === "11" && grid[id-1].size === "12")	
-					swap([id+1,id,id-1]);//"12"right above "11"at id and another "11"right from id
-				else if (grid[id].size === "13")
-					click_right(id-1);
-				else if (grid[id].size === "11" && grid[id-1].size === "13")
-					swap([id,id-1]);//"11","12"
-				else
-					changed = false;
+				swap([id,id-1]);
+				changed = true;
 			}
 			else if (view === "horizontal")
 				click_right(id-1);//TODO f.e. "44" jump
@@ -541,32 +496,10 @@
 		id = Number(id);
 		var changed = false;
 		if (view === "vertical"){
-			if (id < grid.length-1){
-				if (grid[id].size === "14" || grid[id].size === "24" || grid[id].size === "44"|| grid[id].size === "34")
-					click_down(id);
-				else if (id < grid.length){//just for less loopthrough
-				 	changed = true;
-					if ((grid[id].size === "13" && grid[id+1].size === "11" || grid[id].size === "11" && grid[id+1].size === "13") && grid[id].start % 4 === 1)
-						swap([id+1,id]);//"13","11" or "11","13"
-				 	else if (grid[id].size === "13" && grid[id].stop % 4 === 0 && grid[id+1].size === "11" &&  grid[id+2].size === "11" && grid[id+3].size === "11")
-				 		swap([id,id+3,id+2,id+1]);//13,11,11,11
-				 	else if (grid[id].size === "13" && grid[id].stop % 4 === 0 && (
-				 		grid[id+1].size === "11" &&  grid[id+2].size === "12" ||//13,11,12
-				 		grid[id+1].size === "12" &&  grid[id+2].size === "11"))//13,12,11
-				 		swap([id,id+2,id+1]);
-				 	else if ((grid[id].size === grid[id+1].size) || (grid[id].size === "11" && grid[id+1].size === "12" && grid[id].stop % 4 != 0) || (grid[id].size === "12" && grid[id+1].size === "11" && grid[id].stop % 4 != 0))	
-						swap([id+1,id]);//if left right one or row above right one are the same size
-					else if (grid[id].size === "12" && grid[id+1].size === "11" && grid[id+2].size === "11" && grid[id+1].start % 4 === 1)
-						swap([id+2,id+1,id]);//id 12 and left or the right 2 in row above are 11
-					else if (grid[id].size === "11" && grid[id+1].size === "12" && grid[id-1].size === "11" && grid[id+1].start % 4 === 1)
-						swap([id-1,id,id+1]);// id "11"left "11"and below left "12"
-					else if (grid[id].size === "11" && grid[id].start % 4 === 0 && grid[id-1].size === "11" && grid[id-2].size === "11" && grid[id+1].size === "13")
-						swap([id+1,id-2,id-1,id]);
-					else if(grid[id+1].size === "13" && grid[id].stop % 4 === 0 && (grid[id].size === "11" && grid[id-1].size === "12" || grid[id].size === "12" && grid[id-1].size === "11"))
-						swap([id+1,id-1,id]);
-					else 
-						changed = false;		
-				}
+			if (id < grid.length-2){
+				swap([id,id+1]);
+				changed = true;
+
 			}
 		}
 		else if (view === "horizontal"){
@@ -623,7 +556,6 @@
 		}
 		if (changed)
 			grid.update();
-		
 	}
 
 	//---- ok ----
@@ -631,7 +563,7 @@
 		mode = "show";
 		for (i=grid.length-1;i>0;i--){//delete all dummies are at the end
 			//begins at the last element
-			if (grid[i].name != "dummy"){
+			if (grid[i].type != "dummy"){
 				grid.splice(i+1,grid.length-(i-1));
 				i=0;
 			}
@@ -641,7 +573,7 @@
 
 	//---- delete  ----
 	function click_delete(id){
-		if (grid[id].name != "settings" && grid[id].name != "dummy"){
+		if (grid[id].type != "settings" && grid[id].type != "dummy"){
 			if (view === "vertical")
 				grid.splice(id,1);	//dummies are deleted automatically, when they are at the end
 			else if (view =="horizontal")
